96c76190f370a6482e48f94250a3c4d806e7fceb
Oscar Tinajero <tinajero117.oscar@gmail.com>
2022-11-10 00:24:21 -0600

    changes

--- api/connection/connection.php
+++ api/connection/connection.php
@@ -7,7 +7,7 @@
         public function connect(){
             try {
                 //code...
-                $client = new MongoDB\Client(DB_CONNECT);
+                $client = new MongoDB\Client(db_connection);
                 return $client->selectDatabase(DB);
             } catch (\Throwable $th) {
                 //throw $th;
--- api/connection/constants_connection.php
+++ api/connection/constants_connection.php
@@ -1,5 +1,5 @@
 <?php
     const DB = 'ToDoApp';
     # DB Connection
-    const DB_CONNECT = 'mongodb://todoapp-database:98cGmjMQe8Ttqv6F4Svn9X0OwTD6Dj07rDc1fhD3AW2ZmWLyIms90nscsZoVNmHnZXnnIXnub08kACDbd1rwZw==@todoapp-database.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@todoapp-database@';
+    const db_connection = '';
 ?>
