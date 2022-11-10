5a0663b25ac70afa1b80786130cdedac86c795a0
Oscar Tinajero <tinajero117.oscar@gmail.com>
2022-11-10 01:11:20 -0600

    update

--- api/connection.php
+++ api/connection.php
@@ -7,8 +7,8 @@
         public function connect(){
             $DB = 'ToDoApp';
             try {
-                // $db_connection = 'mongodb://todoapp-database:98cGmjMQe8Ttqv6F4Svn9X0OwTD6Dj07rDc1fhD3AW2ZmWLyIms90nscsZoVNmHnZXnnIXnub08kACDbd1rwZw==@todoapp-database.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@todoapp-database@';
-                $db_connection = getenv('db_connection');
+                $db_connection = 'mongodb://todoapp-database:98cGmjMQe8Ttqv6F4Svn9X0OwTD6Dj07rDc1fhD3AW2ZmWLyIms90nscsZoVNmHnZXnnIXnub08kACDbd1rwZw==@todoapp-database.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@todoapp-database@';
+                
                 //code...
                 $client = new MongoDB\Client($db_connection);
                 return $client->selectDatabase($DB);
