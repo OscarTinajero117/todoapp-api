fb643603488b8b6948bbf76f9f45d51ab4fcddb6
Oscar Tinajero <tinajero117.oscar@gmail.com>
2022-11-10 00:31:07 -0600

    change root

--- api/connection/connection.php
+++ api/connection/connection.php
@@ -1,6 +1,6 @@
 <?php  
     
-    require_once("/vendor/autoload.php");
+    require_once($_SERVER['DOCUMENT_ROOT'] ."/todoapp-api/vendor/autoload.php");
     require_once("constants_connection.php");
 
     class Connection{
@@ -9,6 +9,7 @@
                 //code...
                 $client = new MongoDB\Client(db_connection);
                 return $client->selectDatabase(DB);
+
             } catch (\Throwable $th) {
                 //throw $th;
                 return $th->getMessage();
